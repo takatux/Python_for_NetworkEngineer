e9a7dc6b1c886dff872b3fdf13cd2f126be915d7
takatux <alfereszenit@gmail.com>
2019-04-02 14:46:39 +0700

    remote router

--- Python_Telnet.py
+++ Python_Telnet.py
@@ -11,13 +11,13 @@ password = getpass.getpass()
 tn = telnetlib.Telnet(host)
 
 tn.read_until("Username: ")
-tn.write(raw_input('Enter username remote router: '))
+tn.write(user + "\n")
 if password:
     tn.read_until('Password: ')
-    tn.write(getpass.getpass())
+    tn.write(password + "\n")
 
 tn.write("conf t\n")
-tn.write("int f0/0\n")
+tn.write("int f0/1\n")
 tn.write("ip add 192.168.2.1 255.255.255.0\n")
 tn.write("no sh\n")
 tn.write("exit\n")
